8e763e5e978a701b3a3b983f01b0d1d4d4c4340b
zmaltzin <a01794538@tec.mx>
2024-10-13 20:29:30 -0500

    Agregando EDA notebook refactorizada

--- turkish_music_emotion/dataset.py
+++ turkish_music_emotion/dataset.py
@@ -1,69 +1,55 @@
 from pathlib import Path
 from loguru import logger
 import pandas as pd
-import yaml
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
-from turkish_music_emotion.config import RAW_DATA_DIR, PROCESSED_DATA_DIR, INTERIM_DATA_DIR
+from turkish_music_emotion.config import PROCESSED_DATA_DIR, RAW_DATA_DIR, INTERIM_DATA_DIR
 
 class DataHandler:
     def __init__(self):
         self.data = None
-        self.le = LabelEncoder()
+        self.input_path = None
 
-
-    def load_data(self, input_path: Path, input_filename: str):
-        full_input_path = input_path / input_filename
-        logger.info(f"Loading data from {full_input_path}")
-        
-        self.data = pd.read_csv(full_input_path)
-        logger.success(f"Data loaded successfully from {full_input_path}")
+    def load_raw_data(self, input_path: Path = RAW_DATA_DIR / "Acoustic Features.csv"):
+        self.input_path = input_path
+        logger.info(f"Loading raw data from {self.input_path}")
+        self.data = pd.read_csv(self.input_path)
+        logger.success("Raw data loaded successfully.")
         return self.data
 
-    def save_data(self, output_dir: Path, filename: str):
+    def load_processed_data(self, input_path: Path):
+        self.input_path = input_path
+        logger.info(f"Loading processed data from {self.input_path}")
+        self.data = pd.read_csv(self.input_path)
+        logger.success("Processed data loaded successfully.")
+        return self.data
+
+    def load_interim_data(self, input_path: Path):
+        self.input_path = input_path
+        logger.info(f"Loading interim data from {self.input_path}")
+        self.data = pd.read_csv(self.input_path)
+        logger.success("Interim data loaded successfully.")
+        return self.data
+
+    def save_processed_data(self, filename: str):
         if self.data is None:
             logger.warning("No data to save. Load the data first.")
             return
         
-        output_path = output_dir / filename
-        logger.info(f"Saving data to {output_path}")
-        
+        output_path = PROCESSED_DATA_DIR / filename
+        logger.info(f"Saving processed data to {output_path}")
         self.data.to_csv(output_path, index=False)
-        logger.success(f"Data saved successfully to {output_path}")
+        logger.success("Processed data saved successfully.")
 
-    def process_data(self):
+    def save_interim_data(self, filename: str):
         if self.data is None:
-            logger.warning("No data loaded. Load data before processing.")
+            logger.warning("No data to save. Load the data first.")
             return
-         # Initialize and use MissingValueAnalyzer
-        analyzer = MissingValueAnalyzer(self.data)
-        missing_columns = analyzer.get_missing_columns()
-        analyzer.display_missing_columns()
         
-        # Continue with data processing
-        self.data.dropna(inplace=True)
-        logger.success("Data processed successfully.")
-        return self.data
-    
-    def scale_data(self):
-        if self.data is None:
-            logger.warning("No data loaded. Load data before scaling.")
-            return
-        scaler = StandardScaler()
-        numeric_cols = self.data.select_dtypes(include=['float64', 'int64']).columns
-        self.data[numeric_cols] = scaler.fit_transform(self.data[numeric_cols])
-        logger.success("Data scaled successfully.")
-        return self.data
-
-    def run(self, input_path: Path, input_filename: str, output_filename: str):
-        self.load_data(input_path, input_filename)
-        self.process_data()
-        self.scale_data()
-        self.save_data(PROCESSED_DATA_DIR, output_filename)
-        return self
-
-
+        output_path = INTERIM_DATA_DIR / filename
+        logger.info(f"Saving interim data to {output_path}")
+        self.data.to_csv(output_path, index=False)
+        logger.success("Interim data saved successfully.")
 
 class MissingValueAnalyzer:
     def __init__(self, data):
@@ -113,10 +99,3 @@ class LabelEncoderWrapper:
         label_encoder = self.label_encoders[column]
         self.data[column] = label_encoder.inverse_transform(self.data[column])
 
-
-if __name__ == "__main__":
-    with open('params.yaml', 'r') as f:
-        params = yaml.safe_load(f)
-
-    handler = DataHandler()
-    handler.run(RAW_DATA_DIR, params['data']['input_filename'], params['data']['output_filename'])
